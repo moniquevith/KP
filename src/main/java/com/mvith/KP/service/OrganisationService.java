3a134f96339da043b54030bca8d2878789d498b1
Monique Vith <mvith@Moniques-MacBook-Pro-2.local>
2025-12-04 21:20:02 +1100

    added organisation service level functions

--- src/main/java/com/mvith/KP/service/OrganisationService.java
+++ src/main/java/com/mvith/KP/service/OrganisationService.java
@@ -1,7 +1,9 @@
 package com.mvith.KP.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+import com.mvith.KP.model.Organisation;
 import com.mvith.KP.repository.OrganisationRepository;
 
 @Service
@@ -13,8 +15,26 @@ public class OrganisationService {
     }
 
     // GET all organisations /org
+    public List<Organisation> getOrganisations() {
+        return organisationRepository.findAll();
+    }
+
     // GET one organisation /org/{id}
+    public Organisation getOneOrganisation(Long id) {
+        return organisationRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Organisation not found with id: " + id));
+    }
     // POST create organisation /org
-    // PATCH update an organisation details /org
+    public Organisation createOrganisation(Organisation organisation) {
+        return organisationRepository.save(organisation);
+    }
+
     // DELETE remove organisation
+    public void deleteOrganisation(Long id) {
+        organisationRepository.deleteById(id);
+    }
+
+    // PATCH update an organisation details /org
+    
+
 }

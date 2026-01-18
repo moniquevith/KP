80b1f5e4711ae2144c2113bae679a912bd1c149a
Monique Vith <moniquevith1@gmail.com>
2026-01-18 15:09:55 +1100

    completed program dto and service layer

---
+++ src/main/java/com/mvith/KP/dto/ProgramCreateRequest.java
@@ -0,0 +1,69 @@
+package com.mvith.KP.dto;
+
+import com.mvith.KP.model.Organisation;
+import com.mvith.KP.model.ProgramType;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class ProgramCreateRequest {
+    private Integer age_min;
+    private Integer age_max;
+    private String name;
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private ProgramType type_id;  
+
+    @ManyToOne
+    @JoinColumn(name = "org_id")
+    private Organisation org_id; 
+
+    public Integer getAge_Min() {
+        return age_min;
+    }
+
+    public void setAge_max(Integer age) {
+        this.age_max = age;
+    }
+
+    public Integer getAge_Max() {
+        return age_max;
+    }
+
+    public void setAge_min(Integer age) {
+        this.age_min = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ProgramType getTypeID() {
+        return type_id;
+    }
+
+    public void setTypeID(ProgramType type_id) {
+        this.type_id = type_id;
+    }
+
+    public Organisation getOrgID() {
+        return org_id;
+    }
+
+    public void setOrg_id(Organisation org_id) {
+        this.org_id = org_id;
+    }
+}
--- src/main/java/com/mvith/KP/service/ProgramService.java
+++ src/main/java/com/mvith/KP/service/ProgramService.java
@@ -2,6 +2,8 @@ package com.mvith.KP.service;
 
 import java.util.List;
 
+import com.mvith.KP.dto.ProgramCreateRequest;
+import com.mvith.KP.dto.ProgramPatchRequest;
 import com.mvith.KP.model.Program;
 import com.mvith.KP.repository.ProgramRepository;
 
@@ -28,6 +30,55 @@ public class ProgramService {
     }
 
     // POST create program /program
+    public Program createProgram(ProgramCreateRequest request) {
+        Program prog = new Program();
+
+        prog.setAge_max(request.getAge_Max());
+        prog.setAge_min(request.getAge_Min());
+        prog.setDescription(request.getDescription());
+        prog.setName(request.getName());
+        prog.setOrg_id(request.getOrgID());
+        prog.setType_id(request.getTypeID());
+
+        return programRepository.save(prog);
+    }
+
     // PATCH update program /program
+    public Program patchProgram(Long id, ProgramPatchRequest updates) {
+        Program prog = programRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Program not found with id: " + id));
+
+        if (updates.getAge_Max() != null) {
+            prog.setAge_max(updates.getAge_Max());
+        }
+
+        if (updates.getAge_Min() != null) {
+            prog.setAge_min(updates.getAge_Min());
+        }
+
+        if (updates.getDescription() != null) {
+            prog.setDescription(updates.getDescription());
+        }
+
+        if (updates.getName() != null) {
+            prog.setName(updates.getName());
+        }
+
+        if (updates.getOrgID() != null) {
+            prog.setOrg_id(updates.getOrgID());
+        }
+
+        if (updates.getTypeID() != null) {
+            prog.setType_id(updates.getTypeID());
+        }
+
+        return programRepository.save(prog);
+    }
+
     // DELETE remove program /program
+    public void deleteProgram(Long id) {
+        programRepository.deleteById(id);
+    }
+
+
 }
